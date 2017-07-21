757b6f5eb7fafe2feee6395011e80ddcc44ba66d
Mormoran <mormoran@gmail.com>
2017-07-21 14:48:32 +0100

    Disabled evnrionment variables

--- rich-e-commerce/settings.py
+++ rich-e-commerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import dj_database_url
-from .env import *
+# from .env import *
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -90,15 +90,15 @@ WSGI_APPLICATION = 'rich-e-commerce.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL")
-DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
+# CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL")
+# DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)
 
 
 # Password validation
